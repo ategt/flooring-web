b6227f47f30ac1d5e46e7ca935c87311de45b10a
ATegtmeier <ategtmei@gmail.com>
2017-05-03 18:39:31 -0400

    Audit aspect test passing.

--- src/test/java/com/mycompany/flooringmasteryweb/aop/AuditAspectTest.java
+++ src/test/java/com/mycompany/flooringmasteryweb/aop/AuditAspectTest.java
@@ -7,12 +7,15 @@ package com.mycompany.flooringmasteryweb.aop;
 
 import com.mycompany.flooringmasteryweb.dao.AuditDao;
 import com.mycompany.flooringmasteryweb.dao.OrderDao;
+import com.mycompany.flooringmasteryweb.dto.Audit;
 import com.mycompany.flooringmasteryweb.dto.Order;
 import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -53,12 +56,12 @@ public class AuditAspectTest {
     @Test
     public void testCreateAuditEntry() throws Exception {
         System.out.println("createAuditEntry");
-        
+
         Random random = new Random();
-        
+
         Order order = new Order();
-        
-        order.setDate(new Date());
+
+        order.setDate(new Date(System.currentTimeMillis() - 25000));
         order.setId(-Math.abs(random.nextInt()));
         order.setTotal(random.nextDouble());
         order.setName(UUID.randomUUID().toString());
@@ -66,8 +69,18 @@ public class AuditAspectTest {
         OrderDao orderDao = ctx.getBean("orderDao", OrderDao.class);
 
         orderDao.create(order);
-        
-        AuditDao auditDao = ctx.getBean("auditAspect", AuditDao.class);
-        auditDao.
+
+        AuditDao auditDao = ctx.getBean("auditDao", AuditDao.class);
+        int lastAuditPosition = auditDao.getSize();
+        Audit lastAudit = auditDao.getResultRange(lastAuditPosition - 1, 1).get(0);
+
+        assertTrue(Math.abs(lastAudit.getDate().getTime() - order.getDate().getTime()) - 25000 < 50);
+
+        assertEquals(lastAudit.getActionPerformed(), "create");
+        assertEquals(lastAudit.getOrderid(), order.getId());
+        assertTrue(Math.abs(lastAudit.getLogDate().getTime() - System.currentTimeMillis()) < 50);
+        assertEquals(lastAudit.getOrderName(), order.getName());
+        assertEquals(lastAudit.getOrderTotal(), order.getTotal(), 100);
+
     }
 }
