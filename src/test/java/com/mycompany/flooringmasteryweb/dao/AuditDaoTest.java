356f2a7b528a3b2fcb903c68f55ac52efbafff3a
ATegtmeier <ategtmei@gmail.com>
2017-05-04 20:09:46 -0400

    About to clean out the old tests.

--- src/test/java/com/mycompany/flooringmasteryweb/dao/AuditDaoTest.java
+++ src/test/java/com/mycompany/flooringmasteryweb/dao/AuditDaoTest.java
@@ -78,7 +78,7 @@ public class AuditDaoTest {
     public void testCreateWithGenerator() {
         System.out.println("create");
         Audit audit = auditGenerator();
-        
+
         Audit result = instance.create(audit);
         assertEquals(audit, result);
     }
@@ -87,67 +87,77 @@ public class AuditDaoTest {
     public void testCreateAndGet() {
         System.out.println("create and get");
         Audit audit = auditGenerator();
-        
+
         int size = instance.getSize();
         Audit result = instance.create(audit);
         assertEquals(audit, result);
-        
+
         assertEquals(size + 1, instance.getSize());
         assertEquals(size + 1, instance.getSize());
-        
+
         Audit returned = instance.get(result.getId());
         assertTrue(returned.equals(result));
         assertEquals(returned, result);
 
         assertEquals(size + 1, instance.getSize());
-        
+
         List<Audit> audits = instance.get();
-        
+
         assertTrue(audits.contains(returned));
         assertTrue(audits.contains(result));
         assertEquals(audits.size(), instance.getSize());
     }
 
     @Test
-    public void testPagination(){
-        List<Audit> audits = instance.get();
+    public void testPagination() {
         
+        if (instance.getSize() < 25) {
+            List<Audit> createdAudits = new ArrayList();
+            for (int i = 0; i < 25; i++) {
+                createdAudits.add(instance.create(auditGenerator()));
+            }
+        }
+        
+        List<Audit> audits = instance.get();
+
         int totalSize = audits.size();
         List<Audit> firstAudits = instance.getResultRange(0, 10);
         List<Audit> secondAudits = instance.getResultRange(10, 10);
         List<Audit> thirdAudits = instance.getResultRange(20, 10);
-        
-        assertEquals(instance.getResultRange(20, 16).size(), 16);
-        
+
+        List<Audit> results = instance.getResultRange(20, 16);
+        int resultsSize = results.size();
+        assertEquals(resultsSize, 16);
+
         List<Audit> cummulativeAudits = new ArrayList();
         cummulativeAudits.addAll(firstAudits);
         cummulativeAudits.addAll(secondAudits);
         cummulativeAudits.addAll(thirdAudits);
-        
+
         assertEquals(cummulativeAudits.size(), 30);
-        
+
         for (int i = 0; i < cummulativeAudits.size(); i++) {
-            assertEquals(cummulativeAudits.get(i), audits.get(i));            
+            assertEquals(cummulativeAudits.get(i), audits.get(i));
         }
 
         List<Audit> paginatedAudits = instance.getWithPagination(2, 10);
-        
+
         assertEquals(paginatedAudits.size(), 10);
-        
+
         for (int i = 0; i < paginatedAudits.size(); i++) {
             assertEquals(paginatedAudits.get(i), thirdAudits.get(i));
         }
-        
+
         List<Audit> endingAudits = instance.getResultRange(totalSize - 5, 10);
         assertEquals(endingAudits.size(), 5);
-        
+
         for (int i = 0; i < endingAudits.size(); i++) {
             assertEquals(endingAudits.get(i), audits.get(audits.size() - 5 + i));
         }
     }
-    
+
     private Audit auditGenerator() {
-        
+
         Audit instance = new Audit();
         Random random = new Random();
         instance.setActionPerformed("action");
@@ -155,8 +165,8 @@ public class AuditDaoTest {
         instance.setId(random.nextInt());
         instance.setLogDate(new Date(32500000L));
         instance.setOrderName("Order Name");
-        instance.setOrderTotal(random.nextInt(20000)/100);
-        instance.setOrderid(random.nextInt(10000)/100);
+        instance.setOrderTotal(random.nextInt(20000) / 100);
+        instance.setOrderid(random.nextInt(10000) / 100);
         return instance;
     }
 
