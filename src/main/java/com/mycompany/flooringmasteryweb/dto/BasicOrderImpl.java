6c95813e1759e6fb12d4374727f8e3de0642ea1b
ATegtmeier <ategtmei@gmail.com>
2017-05-19 21:12:51 -0400

    Still do not have this cleaned up quite right.

---
+++ src/main/java/com/mycompany/flooringmasteryweb/dto/BasicOrderImpl.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.flooringmasteryweb.dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author apprentice
+ */
+public class BasicOrderImpl implements BasicOrder {
+    private int id;
+    private String name;
+    private String state;
+    private String product;
+    private Date date;
+    private double area;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the product
+     */
+    public String getProduct() {
+        return product;
+    }
+
+    /**
+     * @param product the product to set
+     */
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the area
+     */
+    public double getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(double area) {
+        this.area = area;
+    }
+}
