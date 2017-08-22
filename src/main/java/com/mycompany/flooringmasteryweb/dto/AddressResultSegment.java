2894014fc127813948ddd7839bb51236b93cb3b8
Adam Tegtmeier <ategtmei@gmail.com>
2017-08-22 11:13:50 -0400

    Working to restore the search request functions.

---
+++ src/main/java/com/mycompany/flooringmasteryweb/dto/AddressResultSegment.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.flooringmasteryweb.dto;
+
+/**
+ *
+ * @author ATeg
+ */
+public class AddressResultSegment {
+    private Integer page;
+    private Integer resultsPerPage;
+    
+    private AddressSortByEnum sortBy;
+
+    /**
+     * @return the page
+     */
+    public Integer getPage() {
+        return page;
+    }
+
+    /**
+     * @param page the page to set
+     */
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    /**
+     * @return the resultsPerPage
+     */
+    public Integer getResultsPerPage() {
+        return resultsPerPage;
+    }
+
+    /**
+     * @param resultsPerPage the resultsPerPage to set
+     */
+    public void setResultsPerPage(Integer resultsPerPage) {
+        this.resultsPerPage = resultsPerPage;
+    }
+
+    /**
+     * @return the sortBy
+     */
+    public AddressSortByEnum getSortBy() {
+        return sortBy;
+    }
+
+    /**
+     * @param sortBy the sortBy to set
+     */
+    public void setSortBy(AddressSortByEnum sortBy) {
+        this.sortBy = sortBy;
+    }
+}
---
+++ src/main/java/com/mycompany/flooringmasteryweb/dto/AddressSortByEnum.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.flooringmasteryweb.dto;
+
+/**
+ *
+ * @author ATeg
+ */
+public enum AddressSortByEnum {
+    LAST_NAME,
+    FIRST_NAME,
+    COMPANY,
+    ID,
+    LAST_NAME_INVERSE,
+    FIRST_NAME_INVERSE,
+    COMPANY_INVERSE,
+    ID_INVERSE,
+    DEFAULT;
+}
