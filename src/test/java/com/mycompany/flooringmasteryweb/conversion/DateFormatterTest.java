ac422e72966651722c08fb1137e237ef5f32f08d
Adam Tegtmeier <ategtmei@gmail.com>
2017-09-29 02:16:09 -0400

    Still trying to get formatters to work right.

---
+++ src/test/java/com/mycompany/flooringmasteryweb/conversion/DateFormatterTest.java
@@ -0,0 +1,67 @@
+package com.mycompany.flooringmasteryweb.conversion;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.format.support.FormattingConversionServiceFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Collections;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration()
+@ContextHierarchy({
+        @ContextConfiguration(locations = {"/test-SetupSimulatedProductionEnvironment.xml"}),
+        @ContextConfiguration(locations = {"/spring-persistence.xml"}),
+        @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring-dispatcher-servlet.xml"})
+})
+public class DateFormatterTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private FormatterRegistry registry;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void parse() throws Exception {
+
+        FormattingConversionServiceFactoryBean factoryBean = new FormattingConversionServiceFactoryBean();
+        factoryBean.setFormatters(Collections.emptySet());
+
+
+        FormattingConversionServiceFactoryBean formattingConversionServiceFactoryBean
+                = webApplicationContext.getBean("conversionService", FormattingConversionServiceFactoryBean.class);
+
+        FormattingConversionService formattingConversionService
+                = formattingConversionServiceFactoryBean.getObject();
+
+        String jhgf = formattingConversionService.convert(new Date(), String.class);
+        System.out.println(jhgf);
+
+    }
+
+    @Test
+    public void print() throws Exception {
+    }
+
+}
