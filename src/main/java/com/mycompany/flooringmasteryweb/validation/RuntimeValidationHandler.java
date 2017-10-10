087c2500b1d1b4dedffe24b74fcfb8db97081ff3
Adam Tegtmeier <ategtmei@gmail.com>
2017-10-10 00:08:26 -0400

    Color coding for the order creation form - done.     Clean up the sorting logic for order and address lists - done.     Utilize the executionTime model object - skipped
        Two of three objectives completed.
        Currently using javascript in place of timing model object, no functionality is lost, only simplicity and cleanliness.
    
     I am considering this branch done, and pushing it to the production server in the morning.

--- src/main/java/com/mycompany/flooringmasteryweb/validation/RuntimeValidationHandler.java
+++ src/main/java/com/mycompany/flooringmasteryweb/validation/RuntimeValidationHandler.java
@@ -1,69 +1,36 @@
 package com.mycompany.flooringmasteryweb.validation;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import java.text.ParseException;
 
 @ControllerAdvice
 public class RuntimeValidationHandler {
 
-    @ExceptionHandler(UnparsableDateException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ValidationErrorContainer processValidationErrors(UnparsableDateException Exception){
-        ValidationErrorContainer validationErrorContainer = new ValidationErrorContainer();
-
-        ValidationError validationError = new ValidationError();
-        validationError.setFieldName("Parse Error");
-        validationError.setMessage("Some part of that input could not be understood.");
-        validationErrorContainer.addError(validationError);
-        return validationErrorContainer;
-    }
-
-    //@ExceptionHandler(RuntimeException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ValidationErrorContainer processValidationErrors(RuntimeException runtimeException){
-        if (runtimeException.getCause() instanceof UnparsableDateException){
-            return processValidationErrors((UnparsableDateException) runtimeException.getCause());
-        }
-
-        ValidationErrorContainer validationErrorContainer = new ValidationErrorContainer();
-
-        ValidationError validationError = new ValidationError();
-        validationError.setFieldName("Unknown");
-        validationError.setMessage("Some part of that input could not be understood.");
-        validationErrorContainer.addError(validationError);
-        return validationErrorContainer;
-    }
-
-    @ExceptionHandler(JsonMappingException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ValidationErrorContainer processValidationErrors(ParseException parseException){
-        ValidationErrorContainer validationErrorContainer = new ValidationErrorContainer();
-
-        ValidationError validationError = new ValidationError();
-        validationError.setFieldName("Parse Error");
-        validationError.setMessage("Some part of that input could not be understood.");
-        validationErrorContainer.addError(validationError);
-        return validationErrorContainer;
-    }
-
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ValidationErrorContainer processValidationErrors(HttpMessageNotReadableException httpMessageNotReadableException){
+    public ValidationErrorContainer processValidationErrors(HttpMessageNotReadableException httpMessageNotReadableException) {
         ValidationErrorContainer validationErrorContainer = new ValidationErrorContainer();
 
         ValidationError validationError = new ValidationError();
-        validationError.setFieldName("Parse Error");
-        validationError.setMessage("Some part of that input could not be understood.");
+
+        Throwable rootCause = httpMessageNotReadableException.getRootCause();
+
+        if (rootCause instanceof UnparsableDateException) {
+            UnparsableDateException unparsableDateException = (UnparsableDateException) rootCause;
+            String format = unparsableDateException.getFormat();
+            String name = unparsableDateException.getName();
+            validationError.setFieldName(name);
+            validationError.setMessage("Date should be in  \"" + format + "\" format.");
+        } else {
+            validationError.setFieldName("Unknown");
+            validationError.setMessage("Some part of that input could not be understood.");
+        }
+
         validationErrorContainer.addError(validationError);
         return validationErrorContainer;
     }
