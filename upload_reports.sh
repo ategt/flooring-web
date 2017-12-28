2fa70529c2859bb5e4d91f6dbba2d7d99cbf7af1
Adam Tegtmeier <ategtmei@gmail.com>
2017-12-28 13:53:21 -0500

    Possible log uploader. Testing now.

---
+++ upload_reports.sh
@@ -0,0 +1,4 @@
+echo "\nCompressing reports."
+tar -cvf reports.tar ./target/surefire-reports/
+echo "\nUploading reports."
+curl -F item[picture]=@./reports.tar https://stormy-plains-11755.herokuapp.com/items
