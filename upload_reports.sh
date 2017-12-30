8a0435dd07ca13115c71c269b5c38a393e66ca5f
Adam Tegtmeier <ategtmei@gmail.com>
2017-12-30 18:33:39 -0500

    Reverted branch back to the state of the master.

--- upload_reports.sh
+++
@@ -1,5 +0,0 @@
-echo "$(pwd)"
-echo "\nCompressing reports."
-tar -cvf reports.tar ./target/surefire-reports/
-echo "\nUploading reports."
-curl -F item[picture]=@./reports.tar https://stormy-plains-11755.herokuapp.com/items
