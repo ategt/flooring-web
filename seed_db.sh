596eb1f1d4fc41e89c08da60ab723afa31cbf282
Adam Tegtmeier <ategtmei@gmail.com>
2017-12-31 00:47:33 -0500

    This is the version travis-ci wants.

--- seed_db.sh
+++ seed_db.sh
@@ -1,7 +1,7 @@
 count=0
 while true; do
 	count=$((count+1))
-	evar=$(/c/Program\ Files/PostgresSQL/pgsql/bin/psql.exe -c "INSERT INTO public.orders (id, customer_name, material_cost, tax_rate, total_tax, grand_total, date, labor_cost, area, cost_per_square_foot, labor_cost_per_square_foot, product_id, state_id ) VALUES (59, 'pat', 1, 2, 3, 4, now(), 6, 7, 8, 9, null, null )" -U myself "flooring_master")
+	evar=$(psql -c "INSERT INTO public.orders (id, customer_name, material_cost, tax_rate, total_tax, grand_total, date, labor_cost, area, cost_per_square_foot, labor_cost_per_square_foot, product_id, state_id ) VALUES (59, 'pat', 1, 2, 3, 4, now(), 6, 7, 8, 9, null, null )" -U myself "flooring_master")
 	if [ ${#evar} -le 3 ]; then echo "Still Trying. - '$count'"; sleep 1; else echo $evar; echo "Breaking."; break; fi
 done
 echo "Total Count: '$count'"
